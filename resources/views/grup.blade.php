370d59ab0520e5e032448c54f2a8e36adece0671
Angga Rizki Maulana <anggarizm.07@gmail.com>
2025-02-05 14:47:21 +0700

    membuat halaman untuk grup

---
+++ resources/views/grup.blade.php
@@ -0,0 +1,17 @@
+@extends('sidebar')
+
+@section('content')
+    <div class="container">
+        <div class="header">
+            <h2>Grup</h2>
+            <a href="/pembuatan_grup" type="button" class="create btn " style="">+
+                Buat Grup
+            </a>
+        </div>
+        <div class="line"></div>
+
+        <div class="text">
+            <p>Kamu Belum Mempunyai Grup</p>
+        </div>
+    </div>
+@endsection
