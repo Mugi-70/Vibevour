a0d28ef6439ca9059d1ffdfb899578480aeb5675
Angga Rizki Maulana <anggarizm.07@gmail.com>
2025-02-13 15:51:36 +0700

    membuat tampilan daftar anggota

--- resources/views/Jadwal/off_canvas/daftar_anggota.blade.php
+++ resources/views/Jadwal/off_canvas/daftar_anggota.blade.php
@@ -1,9 +1,17 @@
-<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+<div class="offcanvas offcanvas-end shadow" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel"
+    data-bs-backdrop="false" style="background-color: #F0F3F8">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+        <h5 class="offcanvas-title" id="offcanvasRightLabel"><i class="bi bi-people-fill me-2"></i><strong>Daftar
+                Anggota (3)</strong></h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
+    <hr>
     <div class="offcanvas-body">
-        ...
+        <div class="input-group shadow-sm">
+            <span class="input-group-text" style="background-color: #ffffff;">
+                <i class="bi bi-search"></i>
+            </span>
+            <input type="text" class="form-control" placeholder="Cari..." style="">
+        </div>
     </div>
 </div>
