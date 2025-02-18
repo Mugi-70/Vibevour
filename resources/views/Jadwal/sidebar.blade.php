60c7cde284f92fdc874acd2b55961e1a948139ce
Angga Rizki Maulana <anggarizm.07@gmail.com>
2025-02-18 16:22:28 +0700

    menambahkan file jQuery

--- resources/views/Jadwal/sidebar.blade.php
+++ resources/views/Jadwal/sidebar.blade.php
@@ -13,6 +13,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
 
     <link rel="stylesheet" href="{{ asset('css/grup.css') }}">
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
