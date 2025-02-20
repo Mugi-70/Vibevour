fce4464c4a60980fedb85ddcd798d31e575e6fab
Angga Rizki Maulana <anggarizm.07@gmail.com>
2025-02-20 12:42:30 +0700

    menambah data

--- app/Http/Controllers/GrupController.php
+++ app/Http/Controllers/GrupController.php
@@ -30,6 +30,25 @@ class GrupController extends Controller
         return view('Jadwal.grup_UI', compact('hadir', 'nama_grup', 'durasi', 'wtku_mulai', 'wtku_selesai', 'tnggl_mulai', 'tnggl_selesai', 'desk', 'tgl', 'times'));
     }
 
+    public function anggota()
+    {
+        $nama_grup = "Jual-Beli";
+        $durasi = "30 menit";
+        $wtku_mulai = "07:00";
+        $wtku_selesai = "09:00";
+        $tnggl_mulai = "01 Januari 2025";
+        $tnggl_selesai = "03 Januari 2025";
+        $desk = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ";
+
+        $tgl = ["1 Januari 2025", "2 Januari 2025", "3 Januari 2025"];
+        $times = ["07:00 - 07:30", "07:30 - 08:00", "08:00 - 08:30", "08:30 - 09:00"];
+
+        $hadir = ["Penjual", "Notaris"];
+
+        return view('Jadwal.grup_UI_anggota', compact('hadir', 'nama_grup', 'durasi', 'wtku_mulai', 'wtku_selesai', 'tnggl_mulai', 'tnggl_selesai', 'desk', 'tgl', 'times'));
+    }
+
     public function pertemuan()
     {
         $anggota = ["Penjual", "Notaris", "Pembeli"];
