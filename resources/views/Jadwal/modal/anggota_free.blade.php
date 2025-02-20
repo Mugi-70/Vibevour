082055766fcb42ba9193cc8fbe9be0967bf4cdd9
Angga Rizki Maulana <anggarizm.07@gmail.com>
2025-02-20 12:43:47 +0700

    tampilan untuk anggota yang menyediakan jadwal

---
+++ resources/views/Jadwal/modal/anggota_free.blade.php
@@ -0,0 +1,32 @@
+<!-- Modal -->
+<div class="modal fade" id="availability_member" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="false">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">2 Januari 2025</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Anggota Bersedia :</p>
+                <div class="box-item d-flex">
+                    <div class="avatar-search">p</div>
+                    <div class="nama-user mt-1">
+                        <h6>Pembeli</h6>
+                        <p style="margin-top: -10px">Pembeli@gmail.com</p>
+                    </div>
+                </div>
+
+                <div class="box-item d-flex">
+                    <div class="avatar-search">p</div>
+                    <div class="nama-user mt-1">
+                        <h6>Notaris</h6>
+                        <p style="margin-top: -10px">Notaris@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
