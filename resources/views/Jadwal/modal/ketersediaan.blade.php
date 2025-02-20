028f38da9b6909e0519a61796e28a754f8ba45b2
Angga Rizki Maulana <anggarizm.07@gmail.com>
2025-02-20 12:44:14 +0700

    tampilan untuk memberitahu kehadiran

---
+++ resources/views/Jadwal/modal/ketersediaan.blade.php
@@ -0,0 +1,20 @@
+<!-- Modal -->
+<div class="modal fade" id="availability" tabindex="-1" aria-labelledby="exampleModalLabel">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel"><strong>Ketersediaan</strong></h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <center>
+                    apakah anda bisa menghadiri pada jadwal ini?
+                </center>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                <button type="button" class="btn btn-primary" onclick="saveSchedule()">Konfirmasi</button>
+            </div>
+        </div>
+    </div>
+</div>
