634271e1f8ebfed50ec4a473ff46eebb96578e4d
Angga Rizki Maulana <anggarizm.07@gmail.com>
2025-03-27 16:11:11 +0700

    membuat menu jadwal pertemuan dan detailnya

--- resources/views/Jadwal/modal/detail_pertemuan.blade.php
+++ resources/views/Jadwal/modal/detail_pertemuan.blade.php
@@ -1,5 +1,6 @@
 <!-- Modal -->
-<div class="modal fade" id="history" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="history-{{ $jadwal->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
             {{-- <div class="modal-header border-0">
@@ -15,7 +16,7 @@
                     <div class="col-md-3 text-center">
                         <div class="d-flex flex-column align-items-center">
                             <i class="bi bi-people-fill fs-1"></i>
-                            <h5 class="fw-bold mt-2">Pembelian Rumah</h5>
+                            <h5 class="fw-bold mt-2" id="modal-judul">{{ $jadwal->judul }}</h5>
                         </div>
                     </div>
 
@@ -23,16 +24,19 @@
                     <div class="col-md-9" style="border-left: 1px solid #ddd">
                         <div class="jp">
                             <p style="font-weight: bold; font-size: 20px">Jadwal Pelaksanaan</p>
-                            <p class="ms-3">3 Januari 07:00 s.d. 07:30</p>
+                            <p id="" class="ms-3">
+                                {{ \Carbon\Carbon::parse($jadwal->tanggal)->translatedFormat('d M Y') }},
+                                {{ \Carbon\Carbon::parse($jadwal->waktu_mulai)->translatedFormat('H:i') }}
+                            </p>
                         </div>
-                        <div class="note">
+                        <div class="note d-none">
                             <p style="font-weight: bold; font-size: 20px">Catatan</p>
                             <div class="card p-1">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                             </div>
                         </div>
-                        <div class="created">
+                        <div class="created d-none">
                             <p style="font-weight: bold; font-size: 20px">Dibuat Oleh</p>
                             <div class="box-item d-flex align-items-center">
                                 <div class="avatar-search">p</div>
@@ -43,12 +47,16 @@
                         </div>
                         <div class="member">
                             <p style="font-weight: bold; font-size: 20px">Anggota</p>
-                            {{-- @foreach ($anggota as $item)
-                                <p class="ms-3">{{ $item }}</p>
-                                <hr>
-                            @endforeach --}}
-
+                            @if ($jadwal->peserta->isNotEmpty())
+                                @foreach ($jadwal->peserta as $anggota)
+                                    <p class="ms-3">{{ $anggota->user->name }}</p>
+                                    <hr>
+                                @endforeach
+                            @else
+                                <p class="ms-3 text-muted">Tidak ada anggota.</p>
+                            @endif
                         </div>
+
                     </div>
                 </div>
             </div>
--- resources/views/Jadwal/pertemuan.blade.php
+++ resources/views/Jadwal/pertemuan.blade.php
@@ -1,29 +1,24 @@
 @extends('Jadwal.sidebar')
 
 @section('header')
-    <div class="card border-0 shadow-sm mb-1">
+    <div class="card border-0 shadow-sm">
         <div class="card-body pt-3 pb-3 pe-3 border-0">
-            <div class="d-flex align-items-center">
-                <!-- Tombol Toggle Sidebar di Kiri -->
-                <button class="btn me-2 d-lg-none" id="toggleSidebar" data-bs-toggle="offcanvas"
-                    data-bs-target="#mobileSidebar">
-                    <i class="bi bi-list"></i>
-                </button>
-
-                <h4 class="mt-3" style="font-weight: bold">Pertemuan</h4>
-
-                <button type="button" class="btn" data-bs-toggle="tooltip" data-bs-placement="bottom"
-                    title="Halaman Grup ">
-                    <i class="bi bi-question-circle"></i>
-                </button>
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center">
+                    <button class="btn me-2 d-lg-none" id="toggleSidebar" data-bs-toggle="offcanvas"
+                        data-bs-target="#mobileSidebar">
+                        <i class="bi bi-list"></i>
+                    </button>
+                    <h5 class="mb-0">Pertemuan</h5>
+                </div>
             </div>
         </div>
     </div>
 @endsection
 
 @section('content')
-    <div class="card_meet">
-        <div class="tab-meet d-flex flex-row gap-1 m-2 justify-content-between">
+    <div class="card border-0 shadow-sm">
+        <div class="tab-meet d-flex flex-wrap gap-1 m-2 justify-content-between">
             <ul class="nav nav-underline" style="margin-left: 25px">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Riwayat</a>
@@ -32,56 +27,59 @@
                     <a class="nav-link" aria-current="page" href="#">Link</a>
                 </li>
             </ul>
-            <button class="filter-meet btn btn-dark m-1" type="submit"><i class="bi bi-sliders m-1"></i>Filter</button>
+            <button class="filter-meet btn btn-secondary m-1 d-none" type="submit">
+                <i class="bi bi-sliders m-1"></i>Filter
+            </button>
         </div>
-        <div class="line-meet"></div>
+
         @foreach ($jadwals as $jadwal)
-            <div class="card-riwayat m-3">
-                <div class="card-body d-flex m-2">
-                    <div class="avatar">P</div>
-                    <div class="judul-pertemuan m-2 mt-2">
-                        <h4>{{ $jadwal->jadwal->judul }}</h4>
+            <div class="line-meet"></div>
+            <div class="m-2">
+                <div class="card-body d-flex flex-column flex-md-row align-items-center justify-content-between">
+                    <!-- Avatar & Judul -->
+                    <div class="d-flex align-items-center text-center text-md-start w-100">
+                        <div class="avatar me-2"
+                            style="width: 40px; height: 40px; background-color: #6c5ce7; border-radius: 50%;">
+                        </div>
+                        <h6 class="fw-bold text-truncate m-0" style="max-width: 150px;">
+                            <strong>{{ $jadwal->judul }}</strong>
+                        </h6>
                     </div>
-                    <button type="button" class="detail btn btn-outline-dark ms-auto mt-1" style="height: 40px"
-                        data-bs-toggle="modal" data-bs-target="#history " data-id="{{ $jadwal->id }}">Detail</button>
-                    @include('Jadwal.modal.detail_pertemuan')
+
+                    <!-- Tanggal & Waktu -->
+                    <div class="text-center w-100 my-2 my-md-0">
+                        <h6 class="fw-bold m-0">
+                            {{ \Carbon\Carbon::parse($jadwal->tanggal)->translatedFormat('d M Y') }}
+                        </h6>
+                        <span class="text-muted" style="font-size: 14px;">
+                            {{ \Carbon\Carbon::parse($jadwal->waktu_mulai)->translatedFormat('H:i') }}
+                        </span>
+                    </div>
+
+                    <!-- Tombol Detail -->
+                    <div class="text-md-end w-100 text-center text-md-start">
+                        <button type="button" class="btn btn-sm btn-outline-dark" data-bs-toggle="modal"
+                            data-bs-target="#history-{{ $jadwal->id }}">
+                            <i class="bi bi-display"></i> Details
+                        </button>
+                    </div>
                 </div>
             </div>
+
+
+
+            <!-- CSS Avatar Responsif -->
+            <style>
+                @media (max-width: 768px) {
+                    #avatar-size {
+                        width: 25px !important;
+                        height: 25px !important;
+                    }
+                }
+            </style>
+
+
+            @include('Jadwal.modal.detail_pertemuan')
         @endforeach
     </div>
 @endsection
-
-<script>
-    $(document).ready(function() {
-        $('.detail').click(function() {
-            var jadwalId = $(this).data('id');
-
-            $.ajax({
-                url: '/pertemuan/' + jadwalId,
-                type: 'GET',
-                success: function(data) {
-                    console.log(data); // Debugging
-
-                    $('#history .modal-title').text(data.nama_jadwal);
-                    $('#history .jp p:nth-child(2)').text(data.tanggal + ' ' + data
-                        .waktu_mulai + ' s.d. ' + data.waktu_selesai);
-                    $('#history .note .card').text(data.catatan);
-                    $('#history .created h6').text(data.pembuat.nama);
-
-                    var anggotaHtml = '';
-                    data.anggota.forEach(function(item) {
-                        anggotaHtml += '<p class="ms-3">' + item.nama + '</p> <
-                        hr > ';
-                    });
-
-                    $('#history .member').html(
-                        '<p style="font-weight: bold; font-size: 20px">Anggota</p>' +
-                        anggotaHtml);
-                },
-                error: function() {
-                    alert('Gagal mengambil data jadwal.');
-                }
-            });
-        });
-    });
-</script>
